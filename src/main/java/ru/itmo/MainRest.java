08c8b25bc3464cf5c61f8ed15dcb7c4e586826e0
qaralle <proger16510@gmail.com>
2024-12-16 02:27:47 +0300

    feat: lab7

--- src/main/java/ru/itmo/MainRest.java
+++ src/main/java/ru/itmo/MainRest.java
@@ -1,14 +1,27 @@
 package ru.itmo;
 
+import org.apache.juddi.v3.client.config.UDDIClient;
+import org.apache.juddi.v3.client.transport.Transport;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.uddi.api_v3.*;
+import org.uddi.v3_service.UDDIPublicationPortType;
+import org.uddi.v3_service.UDDISecurityPortType;
 import ru.itmo.config.CarRestApplication;
 
 public class MainRest {
     private static final Logger logger = LoggerFactory.getLogger(MainRest.class);
+
+    private static final String USER = "admin";
+    private static final String PASSWORD = "admin";
+
+    private static final String BUSINESS_NAME = "CarService";
+    private static final String SERVICE_NAME = "CarServiceLab7";
+    private static final String SERVICE_ENDPOINT = "http://localhost:8083/api/cars";
+
 
     public static void main(String[] args) throws Exception {
         int port = 8083;
@@ -25,10 +38,71 @@ public class MainRest {
             logger.info("Starting Jetty on port {}", port);
             server.start();
             logger.info("Jetty started successfully on port {}", port);
+
+            registerService();
+
             server.join();
         } catch (Exception e) {
             logger.error("Error starting Jetty", e);
             throw e;
         }
+    }
+
+    private static void registerService() throws Exception {
+        UDDIClient uddiClient = new UDDIClient("uddi.xml");
+        Transport transport = uddiClient.getTransport("default");
+
+        UDDISecurityPortType security = transport.getUDDISecurityService();
+        UDDIPublicationPortType publish = transport.getUDDIPublishService();
+
+        AuthToken token = auth(security, USER, PASSWORD);
+
+        registerService(publish, token.getAuthInfo());
+    }
+
+    private static AuthToken auth(UDDISecurityPortType security, String user, String pass) throws Exception {
+        GetAuthToken getAuthToken = new GetAuthToken();
+        getAuthToken.setUserID(user);
+        getAuthToken.setCred(pass);
+        return security.getAuthToken(getAuthToken);
+    }
+
+    private static void registerService(UDDIPublicationPortType publish, String authInfo) throws Exception {
+        BusinessEntity be = new BusinessEntity();
+        Name beName = new Name();
+        beName.setValue(BUSINESS_NAME);
+        be.getName().add(beName);
+
+        SaveBusiness sb = new SaveBusiness();
+        sb.getBusinessEntity().add(be);
+        sb.setAuthInfo(authInfo);
+
+        BusinessDetail bd = publish.saveBusiness(sb);
+        String businessKey = bd.getBusinessEntity().get(0).getBusinessKey();
+        System.out.println("Зарегистрирован бизнес: " + BUSINESS_NAME + " c ключом: " + businessKey);
+
+        BusinessService bs = new BusinessService();
+        bs.setBusinessKey(businessKey);
+        Name bsName = new Name();
+        bsName.setValue(SERVICE_NAME);
+        bs.getName().add(bsName);
+
+        BindingTemplate bt = new BindingTemplate();
+        AccessPoint ap = new AccessPoint();
+        ap.setUseType("endPoint");
+        ap.setValue(SERVICE_ENDPOINT);
+        bt.setAccessPoint(ap);
+
+        BindingTemplates bts = new BindingTemplates();
+        bts.getBindingTemplate().add(bt);
+        bs.setBindingTemplates(bts);
+
+        SaveService ss = new SaveService();
+        ss.setAuthInfo(authInfo);
+        ss.getBusinessService().add(bs);
+        ServiceDetail sd = publish.saveService(ss);
+
+        String serviceKey = sd.getBusinessService().get(0).getServiceKey();
+        System.out.println("Зарегистрирован сервис: " + SERVICE_NAME + " c ключом: " + serviceKey);
     }
 }
--- src/main/java/ru/itmo/client/CarRestClient.java
+++ src/main/java/ru/itmo/client/CarRestClient.java
@@ -1,5 +1,10 @@
 package ru.itmo.client;
 
+import org.apache.juddi.v3.client.config.UDDIClient;
+import org.apache.juddi.v3.client.transport.Transport;
+import org.uddi.api_v3.*;
+import org.uddi.v3_service.*;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -7,14 +12,27 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class CarRestClient extends AbstractClient {
-    private static final String BASE_URL = "http://localhost:8083/api/cars";
+    private static final String SERVICE_NAME = "CarServiceLab-100";
+
+    private static final String USER = "admin";
+    private static final String PASSWORD = "admin";
+
+    private String baseUrl;
     private final Client restClient = ClientBuilder.newClient();
+
+    public CarRestClient() throws IllegalAccessException {
+        this.baseUrl = lookupServiceEndpointFromJuddi(SERVICE_NAME);
+
+        if (this.baseUrl == null) {
+            throw new IllegalAccessException("Service not found");
+        }
+    }
 
     @Override
     protected void createCar() {
         CarParameters params = inputCarParameters();
 
-        Response response = restClient.target(BASE_URL)
+        Response response = restClient.target(baseUrl)
                 .request(MediaType.APPLICATION_JSON)
                 .post(Entity.entity(params, MediaType.APPLICATION_JSON));
 
@@ -31,7 +49,7 @@ public class CarRestClient extends AbstractClient {
         Long id = inputId();
         CarParameters params = inputCarParameters();
 
-        Response response = restClient.target(BASE_URL)
+        Response response = restClient.target(baseUrl)
                 .queryParam("id", id)
                 .queryParam("name", params.name)
                 .queryParam("price", params.price)
@@ -52,7 +70,7 @@ public class CarRestClient extends AbstractClient {
         Long id = inputId();
         CarParameters params = inputCarParameters();
 
-        Response response = restClient.target(BASE_URL + "/" + id)
+        Response response = restClient.target(baseUrl + "/" + id)
                 .request(MediaType.APPLICATION_JSON)
                 .put(Entity.entity(params, MediaType.APPLICATION_JSON));
 
@@ -67,7 +85,7 @@ public class CarRestClient extends AbstractClient {
     protected void deleteCar() {
         Long id = inputId();
 
-        Response response = restClient.target(BASE_URL + "/" + id)
+        Response response = restClient.target(baseUrl + "/" + id)
                 .request()
                 .delete();
 
@@ -76,5 +94,54 @@ public class CarRestClient extends AbstractClient {
         } else {
             System.out.println("Ошибка при удалении автомобиля: " + response.readEntity(String.class));
         }
+    }
+
+    private String lookupServiceEndpointFromJuddi(String serviceName) {
+        try {
+            UDDIClient uddiClient = new UDDIClient("uddi.xml");
+            Transport transport = uddiClient.getTransport("default");
+
+            UDDISecurityPortType security = transport.getUDDISecurityService();
+            UDDIInquiryPortType inquiry = transport.getUDDIInquiryService();
+
+            GetAuthToken getAuthToken = new GetAuthToken();
+            getAuthToken.setUserID(USER);
+            getAuthToken.setCred(PASSWORD);
+            AuthToken token = security.getAuthToken(getAuthToken);
+
+            FindService fs = new FindService();
+            fs.setAuthInfo(token.getAuthInfo());
+            Name searchName = new Name();
+            searchName.setValue(serviceName);
+            fs.getName().add(searchName);
+
+            ServiceList serviceList = inquiry.findService(fs);
+            if (serviceList.getServiceInfos() != null &&
+                    !serviceList.getServiceInfos().getServiceInfo().isEmpty()) {
+                ServiceInfo si = serviceList.getServiceInfos().getServiceInfo().get(0);
+                String serviceKey = si.getServiceKey();
+
+                GetServiceDetail gsd = new GetServiceDetail();
+                gsd.setAuthInfo(token.getAuthInfo());
+                gsd.getServiceKey().add(serviceKey);
+
+                ServiceDetail sd = inquiry.getServiceDetail(gsd);
+
+                if (sd.getBusinessService() != null && !sd.getBusinessService().isEmpty()) {
+                    BusinessService service = sd.getBusinessService().get(0);
+
+                    if (service.getBindingTemplates() != null &&
+                            !service.getBindingTemplates().getBindingTemplate().isEmpty()) {
+                        BindingTemplate bt = service.getBindingTemplates().getBindingTemplate().get(0);
+
+                        return bt.getAccessPoint().getValue();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
